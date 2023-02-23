bef0ef65e950b6f57076540f76e3dce86b60d822
Amin Al Ali Al Darwish <amin@bunnyshell.com>
2023-02-23 08:44:47 +0400

    update deprecated params

--- main.tf
+++ main.tf
@@ -5,9 +5,9 @@ provider "aws" {
 resource "aws_elasticache_replication_group" "elasticache-cluster" {
   availability_zones            	= ["${var.aws_region}a","${var.aws_region}b"]
   replication_group_id          	= "bns-tf-${var.environment}-rep-group"
-  replication_group_description 	= "Bunnyshell ${var.environment} replication group"
+  description 	= "Bunnyshell ${var.environment} replication group"
   node_type                     	= "${var.node_type}"
-  number_cache_clusters         	= "${var.node_count}"
+  num_cache_clusters         	= "${var.node_count}"
   parameter_group_name          	= "default.redis3.2"
   port                          	= 6379
 }
